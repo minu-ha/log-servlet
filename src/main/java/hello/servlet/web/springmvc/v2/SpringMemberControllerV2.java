142b4c8eba881b9863be5441c46be6fe815d0622
wumeenna <minu0604@naver.com>
2022-04-13 04:35:41 +0900

    spring mvc2

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -12,18 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
+@RequestMapping("/springmvc/v2/members")
 public class SpringMemberControllerV2{
 
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
 
-    @RequestMapping( "/springmvc/v2/members/new-form" )
+    @RequestMapping( "/new-form" )
     public ModelAndView newForm(){
         return new ModelAndView("new-form");
     }
 
-    @RequestMapping( "/springmvc/v2/members/save" )
+    @RequestMapping( "/save" )
     public ModelAndView save( HttpServletRequest request , HttpServletResponse response ){
 
         String username = request.getParameter( "username" );
@@ -38,7 +39,7 @@ public class SpringMemberControllerV2{
 
     }
 
-    @RequestMapping( "/springmvc/v2/members" )
+    @RequestMapping( "/members" )
     public ModelAndView members(){
 
         List< Member > members = memberRepository.findAll();
